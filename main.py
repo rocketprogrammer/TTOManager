b9a389a76c2fb71fb5a6852c9bb0ed6499e1ff5b
Rocket <me@rocketprogrammer.me>
2024-01-09 07:21:51 -0600

    main: Create empty `hash_data` folder if it doesn't exist
    * This seems to work for the connection freeze workaround as well

--- main.py
+++ main.py
@@ -15,7 +15,13 @@ params_data = requests.get("https://download.sunrise.games/parameters/parameters
 open(os.path.join(DEFAULT_INSTALL_DIR, "parameters.txt"), "w").write(params_data)
 print("Pointed launcher to Sunrise servers.")
 
-# Next, grab cnc-ddraw
+# Setup the wrokaround for connection hang
+os.makedirs(os.path.join(DEFAULT_INSTALL_DIR, "hash_data"), exist_ok=True)
+print("Added workaround for connection freeze on modern versions of Windows.")
+
+# Finally, grab cnc-ddraw
 ddraw_data = requests.get("https://raw.githubusercontent.com/rocketprogrammer/TTOManager/main/libs/cnc-ddraw/ddraw.dll").content
 open(os.path.join(DEFAULT_INSTALL_DIR, "ddraw.dll"), "wb").write(ddraw_data)
 print("Added cnc-ddraw display wrapper.\nToontown Online should be ready to go now.")
+
+os.system("pause")
